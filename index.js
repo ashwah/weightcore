644efd62a4855e5edd65a4bbcd66bc254edace44
Ashley George <ashley@microserve.io>
2019-01-27 16:59:57 +0000

    Provide a UUID API function. Also some comments for existing code.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// TODO: tidy this section!
 const mysql = require('mysql');
 const express = require('express'),
   bodyParser = require('body-parser'),
@@ -8,6 +9,7 @@ const express = require('express'),
   env = require('./server/config/env'),
   router = require('./server/router/index');
 
+const uuidv1 = require('uuid/v1');
 
 const app = express();
 const PORT = env.PORT;
@@ -18,13 +20,13 @@ var path = require('path');
 var WebSocketServer = require("ws").Server;
 var wss = new WebSocketServer({ port: WS_PORT });
 
+// Define shared headers for app.
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
-// app.use((req, res, next) => {
-//   res.header('Content-Type', 'application/json');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Content-Type', 'application/json');
+  next();
+});
 
 router(app, db, wss);
 
@@ -35,15 +37,25 @@ db.sequelize.sync({ force: true }).then(() => {
   });
 });
 
+// Define a static webroot. All files in this folder are served as a regular web
+// folder.
 app.use(express.static('docroot'))
 
 // Serve the html frontend.
-app.get('/view',function(req,res){
-  res.sendFile(path.join(__dirname+'/docroot/view.html'));
+app.get('/view', function(req, res) {
+  res.sendFile(path.join(__dirname + '/docroot/view.html'));
 });
 app.listen(DOC_PORT);
 
+// Generate a UUID to use as a device ID. This route just provides the IDs.
+app.get('/generate-id', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var uuid = uuidv1();
+  var response = JSON.stringify([uuid])
+  res.send(response);
+});
 
+// Define a response for the Websocket on-connect function.
 wss.on("connection", function(ws) {
   ws.send("Online");
 });
